3fc3706482eba1f6410422dac62c1a651b6c5912
Eric Schulz <eschulz2@gmail.com>
2014-02-08 18:22:53 -0600

    login ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,11 @@
-alert("Hello");
+$(document).ready(function(){
+  console.log("are we on the page?")
+
+ $("#failure").hide(); 
+  $('#login_submit').click(function() {
+   event.preventDefault();
+   $("#failure").show();
+
+  });
+ 
+});
